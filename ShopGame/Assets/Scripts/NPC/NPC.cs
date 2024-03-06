6d31e7f079e09ee2d6c2e60228e025dbe4b18791
ManhTuans <obmhenry12345@gmail.com>
2024-03-06 17:34:20 +0700

    npc path find store

--- ShopGame/Assets/Scripts/NPC/NPC.cs
+++ ShopGame/Assets/Scripts/NPC/NPC.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPC : MonoBehaviour
 {
@@ -11,11 +12,12 @@ public class NPC : MonoBehaviour
     void Start()
     {
         NPCMovement();
+        // MoveToRandomBuilding();
     }
 
     void Update()
     {
-
+        NPCWalkOutOfScreen();
     }
 
     private void NPCMovement()
@@ -28,5 +30,33 @@ public class NPC : MonoBehaviour
 
         Vector3 moveDir = new Vector3(moveDirX, 0, 0);
         rb2D.velocity = moveDir * randomMoveSpeed;
+    }
+
+    private void NPCWalkOutOfScreen()
+    {
+        float xBoundary = 20f;
+        if (transform.position.x < -xBoundary || transform.position.x > xBoundary)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void NPCRotate()
+    {
+
+    }
+
+    private void MoveToRandomBuilding()
+    {
+        Vector2 randomPosition = new Vector2(Random.Range(-10f, 10f), Random.Range(-10f, 10f));
+        transform.position = randomPosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Building"))
+        {
+
+        }
     }
 }
