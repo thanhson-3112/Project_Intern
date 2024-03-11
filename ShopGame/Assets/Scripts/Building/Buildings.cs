f4e051e3243d763553fda15f822e808d721cd77a
Nguyễn Đức Anh <99268985+GLNihilis@users.noreply.github.com>
2024-03-11 10:06:50 +0700

    update

--- ShopGame/Assets/Scripts/Building/Buildings.cs
+++ ShopGame/Assets/Scripts/Building/Buildings.cs
@@ -6,6 +6,8 @@ public class Building : BaseBuilding
 {
     private State state;
     private Level1RecipeSO level1RecipeSO;
+    private Level2RecipeSO level2RecipeSO;
+    private Level3RecipeSO level3RecipeSO;
 
     public enum State
     {
@@ -16,6 +18,8 @@ public class Building : BaseBuilding
     }
 
     [SerializeField] private Level1RecipeSO[] level1RecipeSOArray;
+    [SerializeField] private Level2RecipeSO[] level2RecipeSOArray;
+    [SerializeField] private Level3RecipeSO[] level3RecipeSOArray;
 
     // Start is called before the first frame update
     void Start()
@@ -34,6 +38,8 @@ public class Building : BaseBuilding
 
                     break;
                 case State.Level1:
+                    level1RecipeSO = Level1RecipeSOWithInput(GetBuildingsObject().GetBuildingsObjectSO());
+
                     GetBuildingsObject().DestroyObject();
 
                     BuildingsObject.SpawnBuildingObject(level1RecipeSO.output , this);
@@ -43,27 +49,26 @@ public class Building : BaseBuilding
                     Debug.Log(/*######*/"Upgraded Level1");
                     break;
                 case State.Level2:
+                    level2RecipeSO = Level2RecipeSOWithInput(GetBuildingsObject().GetBuildingsObjectSO());
+
                     GetBuildingsObject().DestroyObject();
 
-                    BuildingsObject.SpawnBuildingObject(level1RecipeSO.output, this);
+                    BuildingsObject.SpawnBuildingObject(level2RecipeSO.output, this);
 
                     state = State.Level3;
                     Debug.Log(/*######*/"Upgraded Level2");
                     break;
                 case State.Level3:
+                    level3RecipeSO = Level3RecipeSOWithInput(GetBuildingsObject().GetBuildingsObjectSO());
+
                     GetBuildingsObject().DestroyObject();
 
-                    BuildingsObject.SpawnBuildingObject(level1RecipeSO.output, this);
+                    BuildingsObject.SpawnBuildingObject(level3RecipeSO.output, this);
 
                     Debug.Log(/*######*/"Upgraded Level3");
                     break;
             }
         }
-    }
-
-    public void Interact()
-    {
-
     }
 
     private Level1RecipeSO Level1RecipeSOWithInput (BuildingsObjectSO inputBuildingsObjectSO)
@@ -73,6 +78,30 @@ public class Building : BaseBuilding
             if (level1RecipeSO.input == inputBuildingsObjectSO)
             {
                 return level1RecipeSO;
+            }
+        }
+        return null;
+    }
+
+    private Level2RecipeSO Level2RecipeSOWithInput(BuildingsObjectSO inputBuildingsObjectSO)
+    {
+        foreach (Level2RecipeSO level2RecipeSO in level2RecipeSOArray)
+        {
+            if (level2RecipeSO.input == inputBuildingsObjectSO)
+            {
+                return level2RecipeSO;
+            }
+        }
+        return null;
+    }
+
+    private Level3RecipeSO Level3RecipeSOWithInput(BuildingsObjectSO inputBuildingsObjectSO)
+    {
+        foreach (Level3RecipeSO level3RecipeSO in level3RecipeSOArray)
+        {
+            if (level3RecipeSO.input == inputBuildingsObjectSO)
+            {
+                return level3RecipeSO;
             }
         }
         return null;
